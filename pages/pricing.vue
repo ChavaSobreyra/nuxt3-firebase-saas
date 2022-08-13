16e46df339c0a2ed27f4aa54b82202714ac9c8d6
Chava Sobreyra <chava.sobreyra@gmail.com>
2022-08-13 12:07:49 -0400

    chore: update readme (#3)

--- pages/pricing.vue
+++ pages/pricing.vue
@@ -1,25 +1,27 @@
 <template>
-  <div>Subscription Plans</div>
-  <template v-if="isLoading">
-    <div
-      class="spinner-border inline-block h-8 w-8 animate-spin rounded-full border-4 text-blue-600"
-      role="status"
-    ></div>
-  </template>
-  <template v-else>
-    <div v-for="product in products" :key="product.id">
-      {{ product.name }}
-      <div v-for="price in product.prices" :key="price.id">
-        ${{ price.unit_amount / 100 }} / {{ price.interval }}
-        <button
-          class="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
-          @click="() => checkout(price)"
-        >
-          Select Price
-        </button>
+  <div>
+    <template v-if="isLoading">
+      <div
+        class="spinner-border inline-block h-8 w-8 animate-spin rounded-full border-4 text-blue-600"
+        role="status"
+      ></div>
+    </template>
+    <template v-else>
+      <div>Subscription Plans</div>
+      <div v-for="product in products" :key="product.id">
+        {{ product.name }}
+        <div v-for="price in product.prices" :key="price.id">
+          ${{ price.unit_amount / 100 }} / {{ price.interval }}
+          <button
+            class="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
+            @click="() => checkout(price)"
+          >
+            Select Price
+          </button>
+        </div>
       </div>
-    </div>
-  </template>
+    </template>
+  </div>
 </template>
 
 <script setup>
@@ -38,6 +40,7 @@ const products = ref([])
 const isLoading = ref(false)
 
 async function fetchProducts() {
+  isLoading.value = true
   const db = getFirestore()
 
   const productsQuerySnapshot = await getDocs(
@@ -53,6 +56,7 @@ async function fetchProducts() {
       prices: pricesQuerySnapshot.docs.map(price => ({ id: price.id, ...price.data() })),
     })
   })
+  isLoading.value = false
 }
 
 if (process.client) fetchProducts()
