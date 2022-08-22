db860f459ab9120240346252e882c65eff57ed73
ChavaSobreyra <chava.sobreyra@gmail.com>
2022-08-21 23:05:44 -0400

    .

--- server/api/subscribe.put.ts
+++ server/api/subscribe.put.ts
@@ -1,15 +1,17 @@
 import mailchimp from '@mailchimp/mailchimp_marketing'
 
+const { mailchimpApiKey, mailchimpServer, mailchimpList } = useRuntimeConfig()
+
 mailchimp.setConfig({
-  apiKey: useRuntimeConfig().mailchimpApiKey,
-  server: useRuntimeConfig().mailchimpServer,
+  apiKey: mailchimpApiKey,
+  server: mailchimpServer,
 })
 
 export default defineEventHandler(async event => {
   try {
     const body = await useBody(event)
 
-    await mailchimp.lists.addListMember(useRuntimeConfig().mailchimpList, {
+    await mailchimp.lists.addListMember(mailchimpList, {
       email_address: body.email,
       status: 'subscribed',
     })
